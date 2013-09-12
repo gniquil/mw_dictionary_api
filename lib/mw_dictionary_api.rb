57ab38861b1bb5f5ab1ddaed376e790e8951306e
Frank Liu <gniquil@gmail.com>
2013-09-12 02:14:25 -0700

    fixed the error of requiring deleted definition classes

--- lib/mw_dictionary_api.rb
+++ lib/mw_dictionary_api.rb
@@ -3,11 +3,9 @@ require 'nokogiri'
 require 'mw_dictionary_api/client'
 require 'mw_dictionary_api/result'
 require 'mw_dictionary_api/entry'
-require 'mw_dictionary_api/definition'
 require "mw_dictionary_api/parsable"
 require 'mw_dictionary_api/entry_parser'
 require 'mw_dictionary_api/result_parser'
-require 'mw_dictionary_api/definition_parser'
 require 'mw_dictionary_api/memory_cache'
 
 module MWDictionaryAPI
